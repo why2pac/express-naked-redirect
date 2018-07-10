89ea4e19d0e39dd3f30e4af0d95d7d90a5ee4352
GONZO <youngyongpark@gmail.com>
2018-07-10 13:24:26 +0900

    Fix lint issues.

--- index.js
+++ index.js
@@ -4,32 +4,31 @@
  * @author GONZO <oss@dp.farm>
  */
 
-
-var subdomainParser = function() {
-  const parseDomain = require('parse-domain');
-  const parsedDomain = {};
+var subdomainParser = (function () {
+  const parseDomain = require('parse-domain')
+  const parsedDomain = {}
 
   return {
-    get: function(hostname) {
+    get: function (hostname) {
       // Cache, Touch
-      if (parsedDomain[hostname] == undefined) {
+      if (parsedDomain[hostname] === undefined) {
         try {
-          var ps = parseDomain(hostname);
-          parsedDomain[hostname] = [ps.subdomain, ps.domain + '.' + ps.tld];
+          var ps = parseDomain(hostname)
+          parsedDomain[hostname] = [ps.subdomain, ps.domain + '.' + ps.tld]
         } catch (e) {
-          parsedDomain[hostname] = [null, null];
+          parsedDomain[hostname] = [null, null]
         }
       }
-      return parsedDomain[hostname];
+      return parsedDomain[hostname]
     }
   }
-}();
+}())
 
-module.exports = function(reverse, subDomain, status) {
-  var options;
+module.exports = function (reverse, subDomain, status) {
+  var options
 
   if (arguments.length === 1 && typeof arguments[0] === 'object') {
-    options = arguments[0];
+    options = arguments[0]
   } else {
     options = {
       reverse: reverse,
@@ -40,74 +39,71 @@ module.exports = function(reverse, subDomain, status) {
 
   if (options.except) {
     if (typeof options.except === 'string') {
-      options.except = [options.except];
+      options.except = [options.except]
     }
 
     // Assertion
     if (typeof options.except.length !== 'number') {
-      throw new Error('Except option must be set with an array or string.');
+      throw new Error('Except option must be set with an array or string.')
     }
 
-    var UrlPattern;
+    var UrlPattern
 
     try {
-      UrlPattern = require('url-pattern');
-    }
-    catch (e) {
+      UrlPattern = require('url-pattern')
+    } catch (e) {
       console.error(e)
-      throw new Error('The third party module `url-pattern` is required.');
+      throw new Error('The third party module `url-pattern` is required.')
     }
 
     for (var i = 0; i < options.except.length; i++) {
-      options.except[i] = new UrlPattern(options.except[i]);
+      options.except[i] = new UrlPattern(options.except[i])
     }
   }
 
-  return function(req, res, next) {
-    var domain = subdomainParser.get(req.hostname);
-    options.subDomain = options.subDomain || 'www';
+  return function (req, res, next) {
+    var domain = subdomainParser.get(req.hostname)
+    options.subDomain = options.subDomain || 'www'
 
     if (options.except) {
       for (var i = 0; i < options.except.length; i++) {
         if (options.except[i].match(req.url)) {
-          return next();
+          return next()
         }
       }
     }
 
-    if (typeof(options.reverse) === 'string' && options.subDomain === undefined) {
-      options.subDomain = options.reverse;
-      options.reverse = false;
+    if (typeof (options.reverse) === 'string' && options.subDomain === undefined) {
+      options.subDomain = options.reverse
+      options.reverse = false
     }
     if (options.status === undefined) {
-      options.status = 302;
+      options.status = 302
     }
 
-    if (typeof(options.protocol) === 'string') {
-      options.protocol = options.protocol;
-    }
-    else if (typeof(options.https) === 'boolean' && options.https) {
-      options.protocol = 'https';
-    }
-    else {
-      options.protocol = req.protocol;
+    if (typeof (options.protocol) === 'string') {
+      options.protocol = options.protocol
+    } else if (typeof (options.https) === 'boolean' && options.https) {
+      options.protocol = 'https'
+    } else {
+      options.protocol = req.protocol
     }
 
-    var redirectTo = null;
+    var redirectTo = null
 
-    if (domain[0] == '' && !options.reverse) {
-      redirectTo = `${options.protocol}://${options.subDomain}.${domain[1]}${req.url}`;
-    } else if (domain[0] == options.subDomain && options.reverse) {
-        redirectTo = `${options.protocol}://${domain[1]}${req.url}`;
+    if (domain[0] === '' && !options.reverse) {
+      redirectTo = `${options.protocol}://${options.subDomain}.${domain[1]}${req.url}`
+    } else if (domain[0] === options.subDomain && options.reverse) {
+      redirectTo = `${options.protocol}://${domain[1]}${req.url}`
     } else if (options.protocol !== req.protocol) {
-      redirectTo = `${options.protocol}://${req.hostname}${req.url}`;
+      redirectTo = `${options.protocol}://${req.hostname}${req.url}`
     }
 
     if (redirectTo !== null) {
-      res.redirect(options.status, redirectTo);
-      return;
+      res.redirect(options.status, redirectTo)
+      return
     }
 
-    return next();
+    return next()
   }
-};
+}
--- test.js
+++ test.js
@@ -1,57 +1,56 @@
-const express = require('express');
-const supertest = require('supertest');
-const assert = require('assert');
-const nakedRedirect = require('./index');
+const express = require('express')
+const supertest = require('supertest')
+const nakedRedirect = require('./index')
 
-var test = function(original, redirect, protocolFrom, protocolTo, opts, bypass, expectCode, willBeIgnored) {
-  const status = opts.status ? opts.status : 302;
-  it(`should redirect ${protocolFrom}://${original} to ${protocolTo}://${redirect} with ${status} status code`, function(done) {
-    var app = express();
+var test = function (original, redirect, protocolFrom, protocolTo, opts, bypass, expectCode, willBeIgnored) {
+  const status = opts.status ? opts.status : 302
+  it(`should redirect ${protocolFrom}://${original} to ${protocolTo}://${redirect} with ${status} status code`, function (done) {
+    var app = express()
 
     if (bypass) {
-      app.use(nakedRedirect(opts));
+      app.use(nakedRedirect(opts))
     } else {
-      app.use(nakedRedirect(opts.rev, opts.sub, opts.status));
+      app.use(nakedRedirect(opts.rev, opts.sub, opts.status))
     }
 
-    app.get('/test', function(req, res) {
-      res.status(200).send(`${req.protocol}://${req.hostname}`);
-    });
+    app.get('/test', function (req, res) {
+      res.status(200).send(`${req.protocol}://${req.hostname}`)
+    })
 
-    var expPrefix = status === 301 ? 'Moved Permanently.' : 'Found.';
-    var expConcat = 'Redirecting to';
-    var expUri = `${protocolTo}://${redirect}/test`;
+    var expPrefix = status === 301 ? 'Moved Permanently.' : 'Found.'
+    var expConcat = 'Redirecting to'
+    var expUri = `${protocolTo}://${redirect}/test`
 
-    var expected = `${expPrefix} ${expConcat} ${expUri}`;
+    var expected = `${expPrefix} ${expConcat} ${expUri}`
 
     if (willBeIgnored) {
-      expected = `${protocolFrom}://${original}`;
+      expected = `${protocolFrom}://${original}`
     }
 
-    supertest(app).get('/test').set('host', original).expect(expected, done);
+    supertest(app).get('/test').set('host', original).expect(expected, done)
   })
 }
 
-describe('express-naked-redirect', function() {
-  test('domain.com', 'www.domain.com', 'http', 'http', {});
-  test('www.domain.com', 'domain.com', 'http', 'http', {rev: true});
-  test('domain.com', 'sub.domain.com', 'http', 'http', {sub: 'sub'});
-  test('sub.domain.com', 'domain.com', 'http', 'http', {rev: true, sub: 'sub'});
-  test('sub.domain.com', 'domain.com', 'http', 'http', {rev: true, sub: 'sub', status: 301});
+describe('express-naked-redirect', function () {
+  test('domain.com', 'www.domain.com', 'http', 'http', {})
+  test('www.domain.com', 'domain.com', 'http', 'http', {rev: true})
+  test('domain.com', 'sub.domain.com', 'http', 'http', {sub: 'sub'})
+  test('sub.domain.com', 'domain.com', 'http', 'http', {rev: true, sub: 'sub'})
+  test('sub.domain.com', 'domain.com', 'http', 'http', {rev: true, sub: 'sub', status: 301})
 
-  test('www.domain.com', 'domain.com', 'http', 'http', {reverse: true}, true);
-  test('domain.com', 'sub.domain.com', 'http', 'http', {subDomain: 'sub'}, true);
-  test('sub.domain.com', 'domain.com', 'http', 'http', {reverse: true, subDomain: 'sub'}, true);
-  test('sub.domain.com', 'domain.com', 'http', 'http', {reverse: true, subDomain: 'sub', status: 301}, true);
+  test('www.domain.com', 'domain.com', 'http', 'http', {reverse: true}, true)
+  test('domain.com', 'sub.domain.com', 'http', 'http', {subDomain: 'sub'}, true)
+  test('sub.domain.com', 'domain.com', 'http', 'http', {reverse: true, subDomain: 'sub'}, true)
+  test('sub.domain.com', 'domain.com', 'http', 'http', {reverse: true, subDomain: 'sub', status: 301}, true)
 
-  test('sub.domain.com', 'domain.com', 'http', 'http', {reverse: true, subDomain: 'sub', status: 301, except: '/tes'}, true);
-  test('sub.domain.com', 'domain.com', 'http', 'http', {reverse: true, subDomain: 'sub', status: 301, except: '/test2'}, true);
-  test('sub.domain.com', 'domain.com', 'http', 'http', {reverse: true, subDomain: 'sub', status: 301, except: ['/test2']}, true);
+  test('sub.domain.com', 'domain.com', 'http', 'http', {reverse: true, subDomain: 'sub', status: 301, except: '/tes'}, true)
+  test('sub.domain.com', 'domain.com', 'http', 'http', {reverse: true, subDomain: 'sub', status: 301, except: '/test2'}, true)
+  test('sub.domain.com', 'domain.com', 'http', 'http', {reverse: true, subDomain: 'sub', status: 301, except: ['/test2']}, true)
 
-  test('sub.domain.com', 'sub.domain.com', 'http', 'http', {reverse: true, subDomain: 'sub', status: 301, except: '/test'}, true, 200, true);
-  test('sub.domain.com', 'sub.domain.com', 'http', 'http', {reverse: true, subDomain: 'sub', status: 301, except: ['/test']}, true, 200, true);
+  test('sub.domain.com', 'sub.domain.com', 'http', 'http', {reverse: true, subDomain: 'sub', status: 301, except: '/test'}, true, 200, true)
+  test('sub.domain.com', 'sub.domain.com', 'http', 'http', {reverse: true, subDomain: 'sub', status: 301, except: ['/test']}, true, 200, true)
 
-  test('domain.com', 'sub.domain.com', 'http', 'https', {protocol: 'https', subDomain: 'sub'}, true, 200);
-  test('sub.domain.com', 'sub.domain.com', 'http', 'https', {protocol: 'https'}, true, 200);
-  test('sub.domain.com', 'sub.domain.com', 'http', 'https', {https: true}, true, 200);
-});
+  test('domain.com', 'sub.domain.com', 'http', 'https', {protocol: 'https', subDomain: 'sub'}, true, 200)
+  test('sub.domain.com', 'sub.domain.com', 'http', 'https', {protocol: 'https'}, true, 200)
+  test('sub.domain.com', 'sub.domain.com', 'http', 'https', {https: true}, true, 200)
+})
